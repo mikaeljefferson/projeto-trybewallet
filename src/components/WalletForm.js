84d36bc6962e019c83530b90530a9bc3e45b0445
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-12-10 15:44:04 -0300

    teste, add algumas actions,table

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,11 +1,175 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteAndUpdateExpense, getCurrencies, saveExpense } from '../redux/actions';
 
-class WalletForm extends Component {
+const ALIMENTACAO = 'Alimentação';
+
+class WalletForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      valueInput: 0,
+      descriptionInput: '',
+      currencyInput: 'USD',
+      methodInput: 'Dinheiro',
+      tagInput: ALIMENTACAO,
+      editForm: false,
+      idEditing: 0,
+    };
+  }
+
+  componentDidMount() {
+    // const { getCurrenciesWallet } = this.props;
+    getCurrenciesWallet();
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
+  addExpenses = () => {
+    // const { saveExpensesWallet, expenses } = this.props;
+    const {
+      valueInput,
+      descriptionInput,
+      currencyInput,
+      methodInput,
+      tagInput,
+    } = this.state;
+    this.setState({
+      valueInput: 0,
+      descriptionInput: '',
+      currencyInput: 'USD',
+      methodInput: 'Dinheiro',
+      tagInput: ALIMENTACAO,
+    }, () => {
+      saveExpensesWallet({
+        valueInput,
+        descriptionInput,
+        currencyInput,
+        methodInput,
+        tagInput,
+        expenses,
+      });
+    });
+  };
+
   render() {
+    const {
+      valueInput,
+      descriptionInput,
+      currencyInput,
+      methodInput,
+      tagInput,
+      editForm,
+      idEditing,
+    } = this.state;
+
+    // const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <div>
+        <form>
+          <label htmlFor="value-input">
+            Valor:
+            <input
+              id="value-input"
+              name="valueInput"
+              data-testid="value-input"
+              type="number"
+              onChange={ this.inputChange }
+              value={ valueInput }
+            />
+          </label>
+          <label htmlFor="description-input">
+            Descrição:
+            <input
+              id="description-input"
+              name="descriptionInput"
+              data-testid="description-input"
+              type="text"
+              onChange={ this.inputChange }
+              value={ descriptionInput }
+            />
+          </label>
+          <label htmlFor="currency-input">
+            Moeda:
+            <select
+              onChange={ this.inputChange }
+              value={ currencyInput }
+              name="currencyInput"
+              id="currency-input"
+              data-testid="currency-input"
+            >
+              {/* {currencies.map((currencie, index) => (
+                <option value={ currencie.toString() } key={ index }>
+                  { currencie }
+                </option>
+              ))} */}
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            Método de pagamento:
+            <select
+              onChange={ this.inputChange }
+              value={ methodInput }
+              name="methodInput"
+              id="method-input"
+              data-testid="method-input"
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag-input">
+            Tag:
+            <select
+              onChange={ this.inputChange }
+              value={ tagInput }
+              name="tagInput"
+              id="tag-input"
+              data-testid="tag-input"
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+          {editForm ? (
+            <button
+              type="button"
+              onClick={ () => this.finishEditing(idEditing) }
+            >
+              Editar despesa
+            </button>
+          ) : (
+            <button type="button" onClick={ this.addExpenses }>Adicionar despesa</button>
+          )}
+        </form>
+      </div>
     );
   }
 }
 
-export default WalletForm;
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getCurrenciesWallet: () => dispatch(getCurrencies()),
+  saveExpensesWallet: (currencie) => dispatch(saveExpense(currencie)),
+  delAndUpdateExpense: (payload) => dispatch(deleteAndUpdateExpense(payload)),
+});
+
+Wallet.propTypes = {
+  currencies: PropTypes.shape(),
+}.isRequired;
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,3 +4,50 @@ export const addPersonalInfo = (value) => ({
   type: ADD_LOGIN_USER,
   payload: value,
 });
+
+export function getCurrencies() {
+  return async (dispatch) => {
+    try {
+      const result = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await result.json();
+      const currencies = Object.entries(data).map((item) => item[0])
+        .filter((item) => item !== 'USDT');
+      dispatch(sucessRequest(currencies));
+    } catch (error) {
+      dispatch(failedRequest(error));
+    }
+  };
+}
+
+export const saveExpense = ({
+  valueInput,
+  descriptionInput,
+  currencyInput,
+  methodInput,
+  tagInput,
+  expenses,
+}) => (
+  async (dispatch) => {
+    try {
+      const result = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await result.json();
+      const expense = [{
+        id: expenses.length,
+        value: valueInput,
+        description: descriptionInput,
+        currency: currencyInput,
+        method: methodInput,
+        tag: tagInput,
+        exchangeRates: data,
+      }];
+      dispatch(sucessRequestSaveExpense(expense));
+    } catch (error) {
+      dispatch(failedRequest(error));
+    }
+  }
+);
+
+export const deleteAndUpdateExpense = (payload) => ({
+  type: 'DELETE_AND_UPDATE_EXPENSE',
+  payload,
+});
