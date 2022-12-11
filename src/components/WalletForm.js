a74b1b24d6ee543861556bdb4fcc49a6dbb9ed86
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-12-11 10:20:59 -0300

    teste se vai walletform

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,11 +1,103 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class WalletForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      valueInput: 0,
+      descriptionInput: '',
+      methodInput: 'Dinheiro',
+      tagInput: 'Alimentação',
+      currencyInput: 'USD',
+    };
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const {
+      valueInput,
+      descriptionInput,
+      methodInput,
+      tagInput,
+      currencyInput,
+    } = this.state;
     return (
-      <div>WalletForm</div>
+      <div>
+        <form />
+        Valor:
+        <input
+          id="value-input"
+          name="valueInput"
+          data-testid="value-input"
+          type="number"
+          onChange={ this.inputChange }
+          value={ valueInput }
+        />
+
+        Descrição:
+        <input
+          id="description-input"
+          name="descriptionInput"
+          data-testid="description-input"
+          type="text"
+          onChange={ this.inputChange }
+          value={ descriptionInput }
+        />
+
+        <select
+          onChange={ this.inputChange }
+          value={ currencyInput }
+          name=" currencyInput"
+          id=" currencyInput"
+          data-testid="currency-input"
+        >
+          {currencies.map((currencie, index) => (
+            <option value={ currencie.toString() } key={ index }>
+              { currencie }
+            </option>
+          ))}
+        </select>
+
+        <select
+          onChange={ this.inputChange }
+          value={ methodInput }
+          name="methodInput"
+          id="method-input"
+          data-testid="method-input"
+        >
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Cartão de crédito">Cartão de crédito</option>
+          <option value="Cartão de débito">Cartão de débito</option>
+        </select>
+
+        <select
+          onChange={ this.inputChange }
+          value={ tagInput }
+          name="tagInput"
+          id="tag-input"
+          data-testid="tag-input"
+        >
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Transporte">Transporte</option>
+          <option value="Saúde">Saúde</option>
+        </select>
+      </div>
+
     );
   }
 }
 
-export default WalletForm;
+const mapStateToProps = (globalState) => ({
+  currencies: globalState.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(WalletForm);
